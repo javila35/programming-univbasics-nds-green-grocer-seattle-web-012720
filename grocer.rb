7b40aa8ecb4c0148bd11fe2fcbed21c9d752e3e2
Joseph Avila <josepharthuravila@gmail.com>
2020-01-08 20:52:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,13 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  index = 0 
+  while index < collection.length do 
+    if collection[index][:item] == name
+      return collection[index]
+    end 
+      index += 1 
+  end 
 end
 
 def consolidate_cart(cart)
@@ -9,18 +16,45 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  array = []
+  index = 0 
+  while index < cart.length do 
+    items_in_cart = find_item_by_name_in_collection(cart[index][:item], array)
+    if items_in_cart
+      items_in_cart[:count] += 1 
+    else
+      items_in_cart = {
+      :item => cart[index][:item],
+      :price => cart[index][:price],
+      :clearance => cart[index][:clearance],
+      :count => 1,
+      }
+      array << items_in_cart
+    end 
+  index += 1 
+  end
+  array 
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  index = 0
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  index = 0 
+  while index < cart.length do
+    if cart[index][:clearance]
+      cart[index][:price] = cart[index][:price] * 0.8
+    end 
+    index += 1 
+  end
+  cart  
 end
 
 def checkout(cart, coupons)
