3408a4780a05ebda1bc1092a054bb611ba3a9a23
Joseph Avila <josepharthuravila@gmail.com>
2020-01-08 21:17:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -30,11 +30,31 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
-  index = 0
-  
+  index = 0 
+  #iterating through the coupons structure
+  while index < coupons.length
+  #finding if our coupon item is in the cart.
+  cart_item = find_item_by_name_in_collection(coupons[index][:item], cart)
+  couponed_item_name = "#{coupons[index][:item]} W/COUPON"
+  cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
+  if cart_item && cart_item[:count] >= coupons[index][:num]
+    if cart_item_with_coupon 
+      cart_item_with_coupon[:count] += coupons[index][:num]
+      cart_item[:count] -= coupons[index][:num]
+    else 
+    cart_item_with_coupon ={
+      :item => couponed_item_name,
+      :price => coupons[index][:cost] / coupons[index][:num],
+      :clearance => cart_item[:clearance],
+      :count => coupons[index][:num]
+    }  
+    cart << cart_item_with_coupon
+    cart_item[:count] -= coupons[index][:num]
+    end 
+  end 
+  index += 1 
+  end
+  cart
 end
 
 def apply_clearance(cart)
